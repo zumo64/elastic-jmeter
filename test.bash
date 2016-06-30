782198da64e9ba1671cb88b5ff695f19ee65fe28
zumo64 <zumo64@gmail.com>
2016-06-30 18:54:48 +0200

    Improved BULK_FILES handling and using 2 queries in demo

--- test.bash
+++ test.bash
@@ -5,21 +5,24 @@ PASS=password
 HOST=192.168.99.100
 PORT=9201
 INDEX=apachelogs-*
-BULK_FILES=21
 QUERY_CSV=input1K1h.csv
 SCROLL_CSV=inputScroll.csv
+SHIELD_ENABLED=YES
 
 # Througput in requests per minute
 QUERY_THROUGHPUT=5.0
 INDEX_THROUGHPUT=2.0
 SCROLL_THROUGHPUT=20.0
 
+# how many files in ./input ?
+BULK_FILES=$(ls -l ./input/* | wc -l)
 
 # Create template
 curl -u "$USER:$PASS" -XPUT "http://$HOST:$PORT/_template/template1" -d @./templates/apache_logs.json
 
+# launch test
 "$JMETER_PATH/bin/jmeter" -n -t ./tests/elk_stress.jmx \
-  -JtestScroll=true  \
+  -JtestScroll=false  \
   -JtestIngest=true  \
   -JtestQuery=true \
   -Jhost="$HOST"  \
